3cb7bca1bc7d667828ebd03024463fe5f2f690f6
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-03-28 00:00:47 +0530

    loops

---
+++ loops/decreasingap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  // int n;
+  // printf("Enter a number:");
+  // scanf("%d", &n);
+  int a = 100;
+  for (int i = 1; a > 0; i++)
+  {
+    printf("%i ", a);
+    a = a - 3;
+  }
+
+  return 0;
+}
--- loops/tableof19.c
+++ loops/tableof19.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
 {
-  for (int i = 1; i <= 190; i++)
+  for (int i = 19; i <= 190; i = i + 19)
   {
-    if (i % 19 == 0)
-    {
-      printf("%d ", i);
-    }
+
+    printf("%d ", i);
   }
   return 0;
 }

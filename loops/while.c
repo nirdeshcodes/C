4bcf68117c2728f77e65da28e827d4254f0d377a
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-03-29 16:08:11 +0530

    while loop

---
+++ loops/while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int i = 1;
+  while (i < 100) //condition lik hi hoti hai ,jab hume jyada cheeze na pata ho
+  {
+    printf("%i ", i);
+    i++;
+  }
+  return 0;
+}

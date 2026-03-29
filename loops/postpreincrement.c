eb5bdaedb8a6da85e7421853d63bbe2a8670e62c
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-03-29 23:46:51 +0530

    loops

---
+++ loops/postpreincrement.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  int x=5;
+  printf("%d\n",x);
+  ++x; 
+  printf("%d\n",x);
+  return 0;
+}
---
+++ loops/sumofdigits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n, lastDigit;
+  printf("Enter a number:");
+  scanf("%d", &n);
+  int sum = 0;
+  while (n != 0)
+  {
+    lastDigit = n % 10;
+    sum = sum + lastDigit;
+    n = n / 10;
+  }
+  printf("Sum of the digits are %d", sum);
+  return 0;
+}

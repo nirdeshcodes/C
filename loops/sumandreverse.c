adba0529f64a9b68b9aa3b3878dcc2a2efa3d5d9
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-04-01 23:40:40 +0530

    sum and reverse of digits

---
+++ loops/sumandreverse.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main()
+{
+  int n, lastDigit;
+  printf("Enter a number:");
+  scanf("%d", &n);
+  int sum = 0;
+  int r = 0;
+  while (n != 0)
+  {
+    lastDigit = n % 10;
+    sum = sum + lastDigit;
+    r = r * 10;
+    r = r + lastDigit;
+    n = n / 10;
+  }
+  printf("Sum of digits: %d\n", sum);
+  printf("Reverse of the number: %d\n", r);
+  return 0;
+}
